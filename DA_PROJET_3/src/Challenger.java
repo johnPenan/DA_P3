871832fd5caaea1a5014ac3915579772b56b9c3d
Jean Penan <jeanpenangoumou1@gmail.com>
2018-05-15 12:56:47 +0200

    Création du projet DA_PROJET_3.

---
+++ DA_PROJET_3/src/Challenger.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Challenger {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int nbreSecret = rand.nextInt(1000 - 1 + 10000);
+
+	}
+
+}
